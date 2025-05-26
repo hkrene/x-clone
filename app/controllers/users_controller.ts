95b6bac96e0139f9cdf371786a3c8a8348d97b75
Hirwa Rene <hirwarene6@gmail.com>
2025-05-26 12:21:56 +0200

    create show and index methodes in user controller

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -1,4 +1,14 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UsersController {
+  public async showProfile({view, response, request}: HttpContext){
+    return view.render('pages/profile')
+  }
+
+  public async store({view}: HttpContext) {
+  }
+
+  public async show({view}: HttpContext) {
+  }
+
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,11 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import UsersController from '#controllers/users_controller'
 
 router.on('/').render('pages/profile')
+
+router.get('/profile', [UsersController, 'showProfile'])
+router.post('posts', [UsersController, 'store'])
+router.get('posts/:id', [UsersController, 'show'])
+
