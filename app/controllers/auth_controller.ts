3d35c7c4ccbb2fa5f84eb623e4474cf73d1c3706
Hirwa Rene <hirwarene6@gmail.com>
2025-06-04 19:55:53 +0200

    created user validator

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,8 +1,11 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import User from '#models/user'
+import mail from '@adonisjs/mail/services/main'
+
 
 export default class AuthController {
-  async store({ request }: HttpContext){
+  
+  async login({ request }: HttpContext){
     const { email, password } = request.only(['email', 'password'])
     const user = await User.verifyCredentials(email, password)
 
