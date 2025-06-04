ec7252d392881edaae003f0f2705185a000ee5fd
Hirwa Rene <hirwarene6@gmail.com>
2025-06-04 13:15:39 +0200

    configured user migration

--- database/migrations/1740667111475_create_users_table.ts
+++ database/migrations/1740667111475_create_users_table.ts
@@ -4,15 +4,22 @@ export default class extends BaseSchema {
   protected tableName = 'users'
 
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
+    this.schema.createTable('users', (table) => {
+      table.increments('id').primary()
+      table.string('username', 50).notNullable().unique()
+      table.string('email', 50).notNullable().unique()
       table.string('password').notNullable()
-
-      table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').nullable()
+      table.string('fullname', 100).nullable()           // Full name
+      table.date('date_of_birth').nullable()             // Date of birth
+      table.string('city', 100).nullable()               // City
+      table.string('website', 255).nullable()            // Website URL
+      table.string('avatar').nullable()                  // Profile image
+      table.string('banner_image').nullable()        // Banner image
+      table.string('bio', 280).nullable()
+      table.timestamps(true)
     })
+    
+    
   }
 
   async down() {
