2b93e81943387902475f58b3d9e4201c1daa0227
Hirwa Rene <hirwarene6@gmail.com>
2025-06-04 19:32:50 +0200

    installed adonis mail dependencies

--- start/routes.ts
+++ start/routes.ts
@@ -10,12 +10,14 @@
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 import UsersController from '#controllers/users_controller'
+import AuthController from '#controllers/auth_controller'
+import TweetsController from '#controllers/posts_controller'
 
 
 
 router.get('/profile', [UsersController, 'showProfile'])
-router.get('home', [UsersController, 'showHome'])
-router.get('posts/:id', [UsersController, 'show'])
+router.get('/home', [UsersController, 'showHome'])
+router.get('/posts/:id', [UsersController, 'show'])
 
 
 router.group(() => {
