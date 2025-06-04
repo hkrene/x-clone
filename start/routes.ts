2407f87a33030ac84a14687641345dcb4b4f3d64
Hirwa Rene <hirwarene6@gmail.com>
2025-06-04 10:38:05 +0200

    added login page

--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,7 @@
 import router from '@adonisjs/core/services/router'
 import UsersController from '#controllers/users_controller'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/login')
 
 router.get('/profile', [UsersController, 'showProfile'])
 router.get('home', [UsersController, 'showHome'])
