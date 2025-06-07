131b2b4320551128b12138eb0ca0b89c1629cbe4
Hirwa Rene <hirwarene6@gmail.com>
2025-06-07 23:16:57 +0200

    :username route

--- app/controllers/profiles_controller.ts
+++ app/controllers/profiles_controller.ts
@@ -1,6 +1,17 @@
+import User from '#models/user'
+import auth from '@adonisjs/auth/services/main'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ProfilesController {
+  public async show({ auth, view }: HttpContext) {
+    const user = await User.findBy('user_id', auth.user?.id)
+    console.log('user', user);
+    
+
+    // Here you would typically fetch the user profile from the database
+    // For now, we will just pass the username to the view
+    return view.render('pages/profile', { user })
+  }
 }
 
 
