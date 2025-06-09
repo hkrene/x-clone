d784d887582cf8ce334fddccb3d10996aed5d42f
Hirwa Rene <hirwarene6@gmail.com>
2025-06-09 14:54:05 +0200

    updated retweet migration

--- database/migrations/1749471147440_create_retweets_table.ts
+++ database/migrations/1749471147440_create_retweets_table.ts
@@ -1,32 +1,18 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
-export default class extends BaseSchema {
+export default class Retweets extends BaseSchema {
   protected tableName = 'retweets'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('tweet_id').unsigned().references('id').inTable('tweets').onDelete('CASCADE')
 
-      table
-        .integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
+      table.unique(['user_id', 'tweet_id']) // Un utilisateur ne peut retweeter un tweet qu'une seule fois
 
-      table
-        .integer('tweet_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('tweets')
-        .onDelete('CASCADE')
-
-      // Empêche un utilisateur de retweeter plusieurs fois le même tweet
-      table.unique(['user_id', 'tweet_id'])
-
-      table.timestamps(true)
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
