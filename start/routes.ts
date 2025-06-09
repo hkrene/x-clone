7a795a60ff33ebd9b60aad80cefbd61964d537ce
Hirwa Rene <hirwarene6@gmail.com>
2025-06-09 12:24:49 +0200

    passed param username to routes

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,29 @@
 |
 */
 
+// import router from '@adonisjs/core/services/router'
+// import { middleware } from './kernel.js'
+// import UsersController from '#controllers/users_controller'
+// import AuthController from '#controllers/auth_controller'
+// import ProfilesController from '#controllers/profiles_controller'
+
+// router.on('/').render('security/login')
+
+// router.get('/signup', [AuthController, 'showSignupForm'])
+// router.post('/signup', [AuthController, 'store'])
+
+// router.get('/login', [AuthController, 'showLoginForm'])
+// router.post('/login', [AuthController, 'login'])
+
+// router.group(() => {
+//   router.get('/home', [UsersController, 'showHome'])
+//   router.get('/profile', [UsersController, 'showProfile'])
+//   router.get('/:username', [ProfilesController, 'show'])
+  
+  
+// }).use(middleware.auth())
+
+
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
 import UsersController from '#controllers/users_controller'
@@ -25,6 +48,5 @@ router.group(() => {
   router.get('/home', [UsersController, 'showHome'])
   router.get('/profile', [UsersController, 'showProfile'])
   router.get('/:username', [ProfilesController, 'show'])
-  
-  
+  router.post('/:username/update', [ProfilesController, 'update']).as('profile.update')
 }).use(middleware.auth())
