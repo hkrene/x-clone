e8ce6cd130971649e03c180237da835ca82c26e5
Hirwa Rene <hirwarene6@gmail.com>
2025-06-09 15:04:18 +0200

    updated likes migration

--- database/migrations/1749471820128_create_likes_table.ts
+++ database/migrations/1749471820128_create_likes_table.ts
@@ -1,32 +1,18 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
-export default class extends BaseSchema {
+export default class Likes extends BaseSchema {
   protected tableName = 'likes'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('tweet_id').unsigned().references('id').inTable('tweets').onDelete('CASCADE')
 
-      table
-        .integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
+      table.unique(['user_id', 'tweet_id']) // Un utilisateur ne peut aimer un tweet qu'une seule fois
 
-      table
-        .integer('tweet_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('tweets')
-        .onDelete('CASCADE')
-
-      // Empêcher les doublons (un like par user par tweet)
-      table.unique(['user_id', 'tweet_id'])
-
-      table.timestamps(true)
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
