8139593127c279951c8ca05cfbd8e93dae980c5d
Hirwa Rene <hirwarene6@gmail.com>
2025-06-09 15:03:40 +0200

    updated comments migration

--- database/migrations/1749471387721_create_comments_table.ts
+++ database/migrations/1749471387721_create_comments_table.ts
@@ -1,31 +1,17 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
-export default class extends BaseSchema {
+export default class Comments extends BaseSchema {
   protected tableName = 'comments'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-
-      table
-        .integer('user_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-
-      table
-        .integer('tweet_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('tweets')
-        .onDelete('CASCADE')
-
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
+      table.integer('tweet_id').unsigned().references('id').inTable('tweets').onDelete('CASCADE')
       table.text('content').notNullable()
 
-      table.timestamps(true)
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
