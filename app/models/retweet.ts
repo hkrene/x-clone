d06194c1a9c17a7a019a68803fb34ace6f275daf
Hirwa Rene <hirwarene6@gmail.com>
2025-06-09 14:28:24 +0200

    updated retweets model

--- app/models/retweet.ts
+++ app/models/retweet.ts
@@ -1,13 +1,29 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+import User from '#models/user'
+import Tweet from '#models/tweet'
 
 export default class Retweet extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare tweetId: number
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Tweet)
+  declare tweet: BelongsTo<typeof Tweet>
 }
