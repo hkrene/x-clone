03ff9d8ea1c4b7368a0c6a19765a83bc2f309d83
Hirwa Rene <hirwarene6@gmail.com>
2025-06-09 14:53:26 +0200

    updated tweet migration

--- database/migrations/1749465819810_create_tweets_table.ts
+++ database/migrations/1749465819810_create_tweets_table.ts
@@ -1,30 +1,16 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
-export default class extends BaseSchema {
+export default class Tweets extends BaseSchema {
   protected tableName = 'tweets'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-
-      // L'utilisateur qui a posté le tweet
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE') // Supprimer les tweets si l'utilisateur est supprimé
-        .notNullable()
-
-      // Contenu du tweet
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.text('content').notNullable()
 
-      // Compteurs
-      table.integer('likes_count').defaultTo(0)
-      table.integer('retweets_count').defaultTo(0)
-      table.integer('comments_count').defaultTo(0)
-
-      table.timestamps(true) // created_at & updated_at gérés automatiquement
+      table.timestamp('created_at', { useTz: true }).defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).defaultTo(this.now())
     })
   }
 
