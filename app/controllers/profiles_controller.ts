8e108d9ab2e403fba12198d728a9477605485c9c
Hirwa Rene <hirwarene6@gmail.com>
2025-06-20 12:15:39 +0200

    created store method to login users

--- app/controllers/profiles_controller.ts
+++ app/controllers/profiles_controller.ts
@@ -9,31 +9,38 @@ export default class ProfilesController {
         return view.render('pages/home')
     }
 
-      public async create({ request, response, auth}: HttpContext) {
-      const data = await request.validateUsing(createUserValidator)
-      
-      const user = await User.create({
-        firstName: data.firstName,
-        surname: data.surname,
-        username: data.username,
-        email: data.email,
-        password: data.password
-      })
+    public async create({ request, response, auth}: HttpContext) {
+    const data = await request.validateUsing(createUserValidator)
+    
+    const user = await User.create({
+    firstName: data.firstName,
+    surname: data.surname,
+    username: data.username,
+    email: data.email,
+    password: data.password
+    })
 
-      const users = await User.verifyCredentials(data.email, data.password)
-      await auth.use('web').login(user)
+    const users = await User.verifyCredentials(data.email, data.password)
+    await auth.use('web').login(user)
 
-      console.log(user);
-      console.log(users);
+    console.log(user);
+    console.log(users);
 
-      // await mail.send((message) => {
-      //   message
-      //     .to(user.email)
-      //     .from('shortenitapp@gmail.com')
-      //     .subject('Verify your email address')
-      //     .htmlView('pages/signup_mail', { user })
-      // })
+    // await mail.send((message) => {
+    //   message
+    //     .to(user.email)
+    //     .from('shortenitapp@gmail.com')
+    //     .subject('Verify your email address')
+    //     .htmlView('pages/signup_mail', { user })
+    // })
 
-      return response.redirect('/home')
-    }
+    return response.redirect('/home')
+}
+
+public async store({ request, auth, response }: HttpContext) {
+     const { email, password } = request.only(['email', 'password'])
+     const user = await User.verifyCredentials(email, password)
+     await auth.use('web').login(user)
+     response.redirect('/home')
+  }
 } 
