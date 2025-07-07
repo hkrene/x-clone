f3f7b8702a1d22fd5aa13318a03cbc415bc4bbe1
Hirwa Rene <hirwarene6@gmail.com>
2025-07-07 17:45:05 +0200

    updated the mail config file

--- config/mail.ts
+++ config/mail.ts
@@ -1,18 +1,18 @@
-import env from '#start/env'
-import { defineConfig, transports } from '@adonisjs/mail'
+// import env from '#start/env'
+// import { defineConfig, transports } from '@adonisjs/mail'
 
-const mailConfig = defineConfig({
-  default: 'smtp',
+// const mailConfig = defineConfig({
+//   default: 'smtp',
 
    /**
     * The mailers object can be used to configure multiple mailers
     * each using a different transport or same transport with different
     * options.
    */
-  mailers: { 
-    smtp: transports.smtp({
-      host: env.get('SMTP_HOST'),
-      port: env.get('SMTP_PORT'),
+  // mailers: { 
+  //   smtp: transports.smtp({
+  //     host: env.get('SMTP_HOST'),
+  //     port: env.get('SMTP_PORT'),
 			/**
        * Uncomment the auth block if your SMTP
        * server needs authentication
@@ -22,8 +22,33 @@ const mailConfig = defineConfig({
         user: env.get('SMTP_USERNAME'),
         pass: env.get('SMTP_PASSWORD'),
       }, */
-    }),
+//     }),
 		     
+//   },
+// })
+
+// export default mailConfig
+
+// declare module '@adonisjs/mail/types' {
+//   export interface MailersList extends InferMailers<typeof mailConfig> {}
+// }
+
+import env from '#start/env'
+import { defineConfig, transports } from '@adonisjs/mail'
+
+const mailConfig = defineConfig({
+  default: 'smtp',
+
+  mailers: {
+    smtp: transports.smtp({
+      host: env.get('SMTP_HOST') as string,
+      port: Number(env.get('SMTP_PORT')),
+      auth: {
+        type: 'login',
+        user: env.get('SMTP_USERNAME') as string,
+        pass: env.get('SMTP_PASSWORD') as string,
+      },
+    }),
   },
 })
 
