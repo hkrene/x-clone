516fa25ac89cd8f054eab96b8b6165a1d26ddef6
Hirwa Rene <hirwarene6@gmail.com>
2025-07-13 16:06:40 +0200

    configured the db

--- config/database.ts
+++ config/database.ts
@@ -1,70 +1,4 @@
-// import env from '#start/env'
-// import { defineConfig } from '@adonisjs/lucid'
 
-// const dbConfig = defineConfig({
-//   connection: 'postgres',
-//   connections: {
-//     postgres: {
-//       client: 'pg',
-//       connection: {
-//         host: env.get('DB_HOST'),
-//         port: env.get('DB_PORT'),
-//         user: env.get('DB_USER'),
-//         password: env.get('DB_PASSWORD'),
-//         database: env.get('DB_DATABASE'),
-//       },
-//       migrations: {
-//         naturalSort: true,
-//         paths: ['database/migrations'],
-//       },
-//     },
-//   },
-// })
-
-// export default dbConfig
-
-// import env from '#start/env'
-// import { defineConfig } from '@adonisjs/lucid'
-
-// const dbConfig = defineConfig({
-//   connection: 'postgres',
-//   connections: {
-//     postgres: {
-//       client: 'pg',
-//       connection: env.get('DATABASE_URL'),
-//       migrations: {
-//         naturalSort: true,
-//         paths: ['database/migrations'],
-//       },
-//     },
-//   },
-// })
-
-// export default dbConfig
-
-// import env from '#start/env'
-// import { defineConfig } from '@adonisjs/lucid'
-
-// const dbConfig = defineConfig({
-//   connection: 'postgres',
-//   connections: {
-//     postgres: {
-//       client: 'pg',
-//       connection: {
-//         connectionString: env.get('DATABASE_URL'),
-//         ssl: { 
-//           rejectUnauthorized: false // This is safe for Railway's setup
-//         }
-//       },
-//       migrations: {
-//         naturalSort: true,
-//         paths: ['database/migrations']
-//       }
-//     }
-//   }
-// })
-
-// export default dbConfig
 
 import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
@@ -97,16 +31,3 @@ export default dbConfig
 
 
 
-// pg: {
-//       client: 'pg',
-//       connection: {
-//         connectionString: url,
-//         ssl: {
-//           rejectUnauthorized: false, // Required by Railway
-//         },
-//       },
-//       migrations: {
-//         naturalSort: true,
-//         paths: ['database/migrations'],
-//       },
-//     },
