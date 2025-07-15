4e16e458238472dae9f8abb9f638851bca714f6e
Hirwa Rene <hirwarene6@gmail.com>
2025-07-15 15:27:21 +0200

    passed user to layout

--- contracts/http.ts
+++ contracts/http.ts
@@ -1,14 +1,14 @@
-// import the **type** HttpContext and alias it to avoid conflict
-import type { HttpContext as BaseHttpContext } from '@adonisjs/core/http'
+// // import the **type** HttpContext and alias it to avoid conflict
+// import type { HttpContext as BaseHttpContext } from '@adonisjs/core/http'
 
-declare module '@adonisjs/core/http' {
-  interface HttpContext extends BaseHttpContext {
-    getSuggestedUsers(): Promise<Array<{
-      id: number
-      firstName: string
-      surname: string
-      username: string
-      avatar: string
-    }>>
-  }
-}
+// declare module '@adonisjs/core/http' {
+//   interface HttpContext extends BaseHttpContext {
+//     getSuggestedUsers(): Promise<Array<{
+//       id: number
+//       firstName: string
+//       surname: string
+//       username: string
+//       avatar: string
+//     }>>
+//   }
+// }
--- start/hooks.ts
+++ start/hooks.ts
@@ -1,30 +1,30 @@
-import { HttpContext } from '@adonisjs/core/http'
-import User from '#models/user'
-import Follow from '#models/follow'
-import { getSignedUrl } from '#services/uploader'
+// import { HttpContext } from '@adonisjs/core/http'
+// import User from '#models/user'
+// import Follow from '#models/follow'
+// import { getSignedUrl } from '#services/uploader'
 
-HttpContext.macro('getSuggestedUsers', async function (this: HttpContext) {
-  const authUser = await this.auth.use('web').authenticate()
+// HttpContext.macro('getSuggestedUsers', async function (this: HttpContext) {
+//   const authUser = await this.auth.use('web').authenticate()
 
-  const followingRecords = await Follow.query()
-    .where('id_user', authUser.id)
-    .select('id_user_following')
+//   const followingRecords = await Follow.query()
+//     .where('id_user', authUser.id)
+//     .select('id_user_following')
 
-  const followingIds = followingRecords.map(record => record.idUserFollowing)
-  followingIds.push(authUser.id)
+//   const followingIds = followingRecords.map(record => record.idUserFollowing)
+//   followingIds.push(authUser.id)
 
-  const users = await User.query()
-    .whereNotIn('id', followingIds)
-    .orderByRaw('RANDOM()')
-    .limit(3)
+//   const users = await User.query()
+//     .whereNotIn('id', followingIds)
+//     .orderByRaw('RANDOM()')
+//     .limit(3)
 
-  return await Promise.all(
-    users.map(async (user) => ({
-      id: user.id,
-      firstName: user.firstName || '',
-      surname: user.surname || '',
-      username: user.username || '',
-      avatar: user.avatar ? await getSignedUrl(user.avatar) : '',
-    }))
-  )
-})
+//   return await Promise.all(
+//     users.map(async (user) => ({
+//       id: user.id,
+//       firstName: user.firstName || '',
+//       surname: user.surname || '',
+//       username: user.username || '',
+//       avatar: user.avatar ? await getSignedUrl(user.avatar) : '',
+//     }))
+//   )
+// })
